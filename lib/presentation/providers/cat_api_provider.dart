72392de897c4b3222d43c189a07dbd5903b94fce
Julian Riveros <juliancamilorifo@ufps.edu.co>
2023-08-16 07:55:10 -0500

    comienzo de lógica consumo de api de gatos

--- lib/presentation/providers/cat_api_provider.dart
+++ lib/presentation/providers/cat_api_provider.dart
@@ -2,5 +2,8 @@ import 'package:cats_breeds/infraestructure/models/cat_model.dart';
 import 'package:flutter/material.dart';
 
 class CatApiProvider extends ChangeNotifier {
-  List<CatModel> cats = [];
+  List<CatModel> _cats = [];
+
+  List<CatModel> get cats => _cats;
+
 }
---
+++ lib/service/cats_service.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'package:cats_breeds/infraestructure/models/cat_model.dart';
+import 'package:http/http.dart' as http;
+
+class CatsService {
+  final String apiKey = "bda53789-d59e-46cd-9bc4-2936630fde39";
+  final String baseUrl = "https://api.thecatapi.com/v1";
+
+  Future<List<CatModel>> fetchCatsData() async {
+    final url = Uri.parse("$baseUrl/breeds");
+    final response = await http.get(
+      url,
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": apiKey,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonData = json.decode(response.body);
+      return jsonData.map((data) => CatModel.fromJson(data)).toList();
+    } else {
+      throw Exception("Failed to load data");
+    }
+  }
+}
