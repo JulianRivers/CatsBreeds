a054862b39935383aa93884c0bdd0b403b1a21db
Julian Riveros <juliancamilorifo@ufps.edu.co>
2023-08-17 09:35:03 -0500

    service image y assets de cat loading

--- lib/src/service/cats_service.dart
+++ lib/src/service/cats_service.dart
@@ -24,4 +24,26 @@ class CatsService {
       throw Exception("Failed to load data");
     }
   }
+
+  Future<String?> validateImageURL(String? imageId) async {
+    final urlJpg = Uri.parse('https://cdn2.thecatapi.com/images/$imageId.jpg');
+    final urlPng = Uri.parse('https://cdn2.thecatapi.com/images/$imageId.png');
+
+    try {
+      final responseJpg = await http.head(urlJpg);
+      if (responseJpg.statusCode == 200) {
+        return urlJpg.toString();
+      }
+
+      final responsePng = await http.head(urlPng);
+      if (responsePng.statusCode == 200) {
+        return urlPng.toString();
+      }
+    } catch (error) {
+      throw('Error: $error');
+    }
+    return 'assets/images/cat_loading.png';
+  }
+  
+
 }
